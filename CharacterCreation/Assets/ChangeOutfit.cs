d5db97295d5561c566fcb5f71e386d0f7e282eb1
kurmahersm <kurmahersm@coventry.ac.uk>
2021-09-28 19:40:52 +0100

    A character creator in Unity

---
+++ CharacterCreation/Assets/ChangeOutfit.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeOutfit : MonoBehaviour
+{
+    public SpriteRenderer bodyPart; // needed reference
+    public List<Sprite> options = new List<Sprite>(); // always new
+    private int OptionAtTheMoment = 0; // which exact option is active
+    public void ChooseNext()
+    {
+        OptionAtTheMoment++; // adds one for next
+        if (OptionAtTheMoment >= options.Count)
+        {
+            OptionAtTheMoment = 0;
+        }
+        bodyPart.sprite = options[OptionAtTheMoment];
+        string test = OptionAtTheMoment.ToString();
+        string test3 = gameObject.name;
+        Debug.Log("Current option is " + test3 + " Clothing option " + test);
+    }
+    public void ChoosePrevious()
+    {
+        OptionAtTheMoment--; // substracts one for previous
+        if (OptionAtTheMoment <= 0)
+        {
+            OptionAtTheMoment = options.Count - 1;
+        }
+        bodyPart.sprite = options[OptionAtTheMoment];
+        string test = OptionAtTheMoment.ToString(); // so it writes the result? yes
+        string test3 = gameObject.name; // chosen object name written in debug.log
+        Debug.Log("Current option is " + test3 + " Clothing option " + test);
+    }
+    public string Randomizer()
+    {
+        OptionAtTheMoment = Random.Range(0, options.Count - 1); // Random sprite from options
+        bodyPart.sprite = options[OptionAtTheMoment];
+        string test = OptionAtTheMoment.ToString(); // To be able to type the name
+        string test3 = gameObject.name;
+        // Debug.Log("Current option is " + test3 + " Clothing option " + test);
+        string AllTest = test + " " + test3;
+        return AllTest;
+    }
+}
---
+++ CharacterCreation/Assets/RandomizerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizerScript : MonoBehaviour
+{
+    public List<ChangeOutfit> ChangeOutfits = new List<ChangeOutfit>();
+    string RandomizerTest;
+    string ForTheMoment;
+    public void CharacterRandomizer()
+    {
+        ForTheMoment = null;
+            foreach (ChangeOutfit changer in ChangeOutfits) // need to go through each object
+            {
+                RandomizerTest = changer.Randomizer();
+                // Debug.Log(RandomizerTest);
+                ForTheMoment = ForTheMoment + " " + RandomizerTest; // Input in line all choices
+            }
+         Debug.Log(ForTheMoment); // Shows the random character at that moment on the screen and the debug.log, when it gets randomized
+    }
+}
